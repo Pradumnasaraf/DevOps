6b0d5d0e31da95b1e20699f730933fda717ce9e8
Pradumna Saraf <pradumnasaraf@gmail.com>
2024-07-27 18:51:49 +0530

    chore: Update social card image in docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -48,7 +48,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/devops-repo-card.png',
       navbar: {
         title: 'DevOps',
         logo: {
